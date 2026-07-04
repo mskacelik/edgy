97696f7402beca0e62b16ac98004a7d04c7fb227
Marek Skacelik <marek.skacelik@gmail.com>
2026-07-04 11:48:33 +0200

    test: implement dev mode hot-reload test with WireMock

--- deployment/src/test/java/org/acme/edgy/test/basic/EdgyDevModeTest.java
+++ deployment/src/test/java/org/acme/edgy/test/basic/EdgyDevModeTest.java
@@ -1,21 +1,67 @@
 package org.acme.edgy.test.basic;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.exactly;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.hamcrest.Matchers.is;
+
+import org.acme.edgy.runtime.api.utils.StatusCode;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
 import io.quarkus.test.QuarkusDevModeTest;
+import io.restassured.RestAssured;
 
 class EdgyDevModeTest {
 
-    // Start hot reload (DevMode) test with your extension loaded
+    static final int CORRECT_WIREMOCK_PORT = 9091;
+
+    private static final String WIREMOCK_RESPONSE = "Hello from WireMock";
+    private static WireMockServer wireMockServer;
+
+    @BeforeAll
+    static void setUp() {
+        wireMockServer = new WireMockServer(options().port(CORRECT_WIREMOCK_PORT));
+        wireMockServer.start();
+        wireMockServer.stubFor(WireMock.get("/api/hello")
+                .willReturn(aResponse().withBody(WIREMOCK_RESPONSE).withStatus(StatusCode.OK)));
+    }
+
     @RegisterExtension
     private static final QuarkusDevModeTest devModeTest = new QuarkusDevModeTest()
-            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(DevModeRoutingProvider.class));
 
-//    @Test
-//    void writeYourOwnDevModeTest() {
-//        // Write your dev mode tests here - see the testing extension guide https://quarkus.io/guides/writing-extensions#testing-hot-reload for more information
-//        Assertions.assertTrue(true, "Add dev mode assertions to " + getClass().getName());
-//    }
+    @Test
+    void testHotReloadRoutingConfiguration() {
+        RestAssured.given()
+                .get("/hello")
+                .then()
+                .statusCode(StatusCode.BAD_GATEWAY);
+        wireMockServer.verify(exactly(0), getRequestedFor(urlEqualTo("/api/hello")));
+
+        devModeTest.modifySourceFile(DevModeRoutingProvider.class,
+                s -> s.replace(DevModeRoutingProvider.INCORRECT_WIREMOCK_PORT, String.valueOf(CORRECT_WIREMOCK_PORT)));
+
+        RestAssured.given()
+                .get("/hello")
+                .then()
+                .statusCode(StatusCode.OK)
+                .body(is(WIREMOCK_RESPONSE));
+        wireMockServer.verify(exactly(1), getRequestedFor(urlEqualTo("/api/hello")));
+    }
+
+    @AfterAll
+    static void tearDown() {
+        wireMockServer.stop();
+    }
 }
